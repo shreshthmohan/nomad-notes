a20246f90061aa9aa946ccb7bf7e8e373057fd9e
Shreshth Mohan <sremog@gmail.com>
2022-05-24 05:38:31 +0530

    suitable places - basic version done

--- src/suitablePlaces.js
+++ src/suitablePlaces.js
@@ -1,15 +1,42 @@
+import { min, max } from 'd3'
+import { processData } from './processData.js'
+
 // places that fit the user selected filter criteria:w
 
 // filters: [{key: 'average high', }]
 import { monthsShort } from '../monthLabels.js'
 
-function suitablePlacesAndMonths(data, comfortTempRange) {
+export function suitablePlacesAndMonths(data, comfortTempRange) {
   // handle case when comfortTempRange is either empty array or undefined: pass-through filter
   const allPlaces = Object.keys(data)
+  // TODO handle case when only one value is provided in comfortTempRange
+  const minComfort = min(comfortTempRange)
+  const maxComfort = max(comfortTempRange)
   const matchingPlacesAndMonths = {}
   allPlaces.forEach(place => {
-    matchingPlacesAndMonths[place] = []
-    monthsShort.forEach(month => {})
-    // data[place]['average high']
+    let matchingMonthsForThisPlace = []
+    monthsShort.forEach(month => {
+      const avgHigh = Number.parseFloat(data[place].data['average high'][month])
+      const avgLow = Number.parseFloat(data[place].data['average low'][month])
+      if (
+        avgHigh < maxComfort &&
+        avgHigh > minComfort &&
+        avgLow > minComfort &&
+        avgLow < maxComfort
+      ) {
+        matchingMonthsForThisPlace.push(month)
+      }
+    })
+    if (matchingMonthsForThisPlace.length) {
+      matchingPlacesAndMonths[place] = matchingMonthsForThisPlace
+    }
   })
+  return matchingPlacesAndMonths
 }
+
+const { weatherData, errors } = processData()
+console.log({ errors })
+
+const matchingWeatherPlaces = suitablePlacesAndMonths(weatherData, [5, 33])
+
+console.log(matchingWeatherPlaces)
